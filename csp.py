cbf8b959b24880a53125fa079a57c5ddb019c916
Student246752 <246752@student.pwr.edu.pl>
2021-03-29 03:45:59 +0200

    Add CSP core function

---
+++ csp.py
@@ -0,0 +1,64 @@
+from typing import Generic, TypeVar, Dict, List, Optional
+from abc import abstractmethod
+
+V = TypeVar('V')
+D = TypeVar('D')
+
+
+class Constraint(Generic[V, D]):
+    def __init__(self, variables: List[V]):
+        self.variables = variables
+
+    @abstractmethod
+    def satisfied(self, assignment: Dict[V, D]) -> bool:
+        pass
+
+
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]):
+        self.variables: List[V] = variables
+        self.domains: Dict[V, List[D]] = domains
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("Every variable should have a domain assigned to it.")
+
+    def add_constraint(self, constraint: Constraint[V, D]):
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint not in CSP")
+            else:
+                self.constraints[variable].append(constraint)
+
+    def consistent(self, variable: V, assignment: Dict[V, D]) -> bool:
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
+
+    def backtracking_search(self, assignment={}, single=False):
+        results = []
+        if len(assignment) == len(self.variables):
+            if single:
+                return assignment
+            return [assignment]
+
+        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+        first: V = unassigned[0]
+        for value in self.domains[first]:
+            local_assignment = assignment.copy()
+            local_assignment[first] = value
+            if self.consistent(first, local_assignment):
+                result = self.backtracking_search(local_assignment, single=single)
+                if result is not None:
+                    if single:
+                        return result
+                    results.extend(result)
+
+        if single:
+            return None
+        if results is not None:
+            return results
+        else:
+            return None
